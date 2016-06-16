17216ad68e8e7c05b1236ad72ec84fa69f252f27
Sander <sandermoolin@gmail.com>
2016-06-16 08:09:50 -0400

    Added boilerplate functions, including iTween, SaFrMo, MenuRefresher, and StringEncryption

---
+++ Template/Assets/Scripts/_snippets/MenuRefresher.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MenuRefresher : MonoBehaviour {
+
+	// MENU REFRESHER
+	// =====================
+	// Utility function to refresh menus, lists, etc.
+	//
+	// Example usage:
+	// If I wanted to create a menu of UI.Texts, but all I have is the array of strings and a prefab of the text, I can:
+	// 1. Assign that prefab to this.itemPrototype
+	// 2. Call MenuRefresher m.Setup<string>(sourceArray, (createdGameObject, sourceString) => { createdGameObject.GetComponent<Text>().text = sourceString; });
+	// 3. THAT'S IT!
+
+	[HideInInspector]
+	public List<GameObject> allCreatedItems = new List<GameObject>();
+
+	/// <summary>
+	/// The item prototype. When Refreshed, a MenuRefresher will create copies of this prototype.
+	/// </summary>
+	public GameObject itemPrototype;
+
+	public delegate void RefreshAction<T>(GameObject go, T sourceReference);
+	public delegate void OnComplete();
+
+	/// <summary>
+	/// Calls initial setup functions on this MenuRefresher, then calls Refresh.
+	/// </summary>
+	/// <param name="sourceArray">Source array.</param>
+	/// <param name="toPerformOnItem">To perform on item.</param>
+	/// <param name="sameParentAsPrototype">If set to <c>true</c> same parent as prototype.</param>
+	/// <param name="keepWorldPosition">If set to <c>true</c> keep world position.</param>
+	/// <param name="deactivatePrototype">If set to <c>true</c> deactivate prototype.</param>
+	/// <typeparam name="T">The 1st type parameter.</typeparam>
+	public List<GameObject> Setup<T>(T[] sourceArray, RefreshAction<T> toPerformOnItem = null, OnComplete onCompleteCallback = null, bool sameParentAsPrototype = true, bool keepWorldPosition = false, bool deactivatePrototype = true) {
+		if (deactivatePrototype)
+			itemPrototype.SetActive(false);
+
+		ClearExistingItems();
+
+		return Refresh<T>(sourceArray, toPerformOnItem, onCompleteCallback, sameParentAsPrototype, keepWorldPosition);
+	}
+
+	/// <summary>
+	/// Create a new GameObject for each T item in sourceArray. Make sure Setup() has been called already.
+	/// </summary>
+	/// <param name="sourceArray">Source array.</param>
+	/// <param name="toPerformOnItem">Action to perform after creating each item. The first parameter represents the GameObject created, the second 
+	/// represents the value in the sourceArray that triggers the GameObject's creation.</param>
+	/// <param name="sameParentAsPrototype">If set to <c>true</c> same parent as prototype.</param>
+	/// <param name="keepWorldPosition">If set to <c>true</c> keep world position.</param>
+	/// <typeparam name="T">The 1st type parameter.</typeparam>
+	public List<GameObject> Refresh<T>(T[] sourceArray, RefreshAction<T> toPerformOnItem = null, OnComplete onCompleteCallback = null, bool sameParentAsPrototype = true, bool keepWorldPosition = false) {
+
+
+		ClearExistingItems();
+		foreach (T t in sourceArray) {
+			CreateNewItemFrom<T>(t, toPerformOnItem, sameParentAsPrototype, keepWorldPosition);
+		}
+		if (onCompleteCallback != null)
+			onCompleteCallback.Invoke();
+		return allCreatedItems;
+	}
+
+	/// <summary>
+	/// Clears the existing items.
+	/// </summary>
+	public void ClearExistingItems() {
+		// Remove any existing created items
+		if (allCreatedItems.Count > 0) {
+			for (int i = 0; i < allCreatedItems.ToArray().Length; i++) {
+				Destroy (allCreatedItems[i]);
+			}
+		}
+		
+		// Clear the list
+		allCreatedItems.Clear();
+	}
+
+	/// <summary>
+	/// Creates the new item from a source reference.
+	/// </summary>
+	/// <returns>The new item from.</returns>
+	/// <param name="sourceReference">Source reference.</param>
+	/// <param name="toPerformOnItem">To perform on item.</param>
+	/// <param name="sameParentAsPrototype">If set to <c>true</c> same parent as prototype.</param>
+	/// <param name="keepWorldPosition">If set to <c>true</c> keep world position.</param>
+	/// <typeparam name="T">The 1st type parameter.</typeparam>
+	public GameObject CreateNewItemFrom<T>(T sourceReference, RefreshAction<T> toPerformOnItem = null,  bool sameParentAsPrototype = true, bool keepWorldPosition = false) {
+		GameObject clonedItem = Instantiate(itemPrototype) as GameObject;
+		clonedItem.SetActive(true);
+		if (sameParentAsPrototype)
+			clonedItem.transform.SetParent(itemPrototype.transform.parent, keepWorldPosition);
+		if (toPerformOnItem != null)
+			toPerformOnItem.Invoke(clonedItem, sourceReference);
+
+		allCreatedItems.Add(clonedItem);
+		return clonedItem;
+	}
+}
---
+++ Template/Assets/Scripts/_snippets/StringEncryption.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Security.Cryptography;
+using System.Text;
+using System;
+
+public static class StringEncryption {
+
+	// From http://www.codeproject.com/Articles/14150/Encrypt-and-Decrypt-Data-with-C
+
+	// STRING ENCRYPTION
+	// Encrypts and decrypts a string according to the given key.
+
+	private static string key = "AcroStorm4Ever";
+
+	public static string Encrypt(string toEncrypt, bool useHashing = false)
+	{
+		byte[] keyArray;
+		byte[] toEncryptArray = UTF8Encoding.UTF8.GetBytes(toEncrypt);
+
+		//If hashing use get hashcode regards to your key
+		if (useHashing)
+		{
+			MD5CryptoServiceProvider hashmd5 = new MD5CryptoServiceProvider();
+			keyArray = hashmd5.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
+			//Always release the resources and flush data
+			// of the Cryptographic service provide. Best Practice
+			hashmd5.Clear();
+		}
+		else
+			keyArray = UTF8Encoding.UTF8.GetBytes(key);
+		
+		TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+		//set the secret key for the tripleDES algorithm
+		tdes.Key = keyArray;
+		//mode of operation. there are other 4 modes.
+		//We choose ECB(Electronic code Book)
+		tdes.Mode = CipherMode.ECB;
+		//padding mode(if any extra byte added)
+		
+		tdes.Padding = PaddingMode.PKCS7;
+		
+		ICryptoTransform cTransform = tdes.CreateEncryptor();
+		//transform the specified region of bytes array to resultArray
+		byte[] resultArray = 
+			cTransform.TransformFinalBlock(toEncryptArray, 0, 
+			                               toEncryptArray.Length);
+		//Release resources held by TripleDes Encryptor
+		tdes.Clear();
+		//Return the encrypted data into unreadable string format
+		return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+	}
+
+	public static string Decrypt(string cipherString, bool useHashing = false)
+	{
+		byte[] keyArray;
+		//get the byte code of the string
+		
+		byte[] toEncryptArray = Convert.FromBase64String(cipherString);
+		
+		if (useHashing)
+		{
+			//if hashing was used get the hash code with regards to your key
+			MD5CryptoServiceProvider hashmd5 = new MD5CryptoServiceProvider();
+			keyArray = hashmd5.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
+			//release any resource held by the MD5CryptoServiceProvider
+			
+			hashmd5.Clear();
+		}
+		else
+		{
+			//if hashing was not implemented get the byte code of the key
+			keyArray = UTF8Encoding.UTF8.GetBytes(key);
+		}
+		
+		TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+		//set the secret key for the tripleDES algorithm
+		tdes.Key = keyArray;
+		//mode of operation. there are other 4 modes. 
+		//We choose ECB(Electronic code Book)
+		
+		tdes.Mode = CipherMode.ECB;
+		tdes.Padding = PaddingMode.PKCS7;
+		
+		ICryptoTransform cTransform = tdes.CreateDecryptor();
+		byte[] resultArray = cTransform.TransformFinalBlock(
+			toEncryptArray, 0, toEncryptArray.Length);
+		//Release resources held by TripleDes Encryptor                
+		tdes.Clear();
+		//return the Clear decrypted TEXT
+		return UTF8Encoding.UTF8.GetString(resultArray);
+	}
+}
