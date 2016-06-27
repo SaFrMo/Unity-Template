a72817c3858f5c0452514a77764f65b724fe9919
Sander <sandermoolin@gmail.com>
2016-06-27 07:27:11 -0400

    Working on Tooltip prefab setup

--- Assets/Scripts/_snippets/Tooltip.cs
+++ Assets/Scripts/_snippets/Tooltip.cs
@@ -5,6 +5,10 @@ using System.Collections;
 namespace SaFrLib {
 	public class Tooltip : MonoBehaviour {
 
+		public Transform toFollow;
+		public Vector3 toFollowOffset;
+		public MenuRefresher exitButtons;
+
 		public Tooltips.TooltipCallback onDestruction;
 
 		void OnDestroy() {
--- Assets/Scripts/_snippets/Tooltips.cs
+++ Assets/Scripts/_snippets/Tooltips.cs
@@ -6,7 +6,7 @@ using System.Collections;
 namespace SaFrLib {
 	public class Tooltips : MonoBehaviour {
 
-		public static string defaultPrefabPath = "SaFrMo/Tooltip";
+		public static string defaultPrefabPath = "SaFrMo/Default Tooltip";
 
 		/// TOOLTIPS
 		/// ===============
@@ -17,14 +17,14 @@ namespace SaFrLib {
 		/// 	Call Tooltips.CreateNew with the following options:
 		/// 	- string toDisplay (required) - the text to display in the Tooltip
 		/// 	- RectTransform parent (optional) - the RectTransform parent of this Tooltip.
-		/// 		Defaults to any Canvas in the scene and creates a Canvas if none is in place.
+		/// 		Defaults to the first Canvas in the scene and creates a Canvas if none is in place.
 		/// 	- Vector3 position (optional) - the center of the spawned Tooltip
 		/// 	- Transform toFollow (optional) - the Transform this tooltip will follow
 		/// 	- Vector3 offset (optional, requires toFollow) - the distance between this
 		/// 		Tooltip and the transform it will follow
 		/// 	- TooltipCallback onCreation (optional) - callback to be fired when this	
 		/// 		Tooltip is created (uses created tooltip as parameter)
-		/// 	- TooltipCallback on (optional) - callback to be fired when this
+		/// 	- TooltipCallback onDestruction (optional) - callback to be fired when this
 		/// 		Tooltip is destroyed (uses created tooltip as parameter)
 		/// 	- string exitText (optional) - text to display on 'Continue...' button (defaults
 		/// 		to 'Continue')
@@ -36,14 +36,17 @@ namespace SaFrLib {
 		/// 
 		/// - TODO
 		/// 	- Style options
-		/// 
+		/// 	- Interaction options (draggable, etc.)
 		/// 
 
 		// Tooltip prefab to instantiate
 		public GameObject prefab;
 		public delegate void TooltipCallback(GameObject createdTooltip);
 	
-		public static void CreateNew(string toDisplay, RectTransform parent = null, TooltipCallback onCreation = null, TooltipCallback onDestruction = null) {
+		public static void CreateNew(	string toDisplay, RectTransform parent = null, Vector3 position = default(Vector3), 
+										Transform toFollow = null, Vector3 offset = default(Vector3), 
+										string exitText = "", string[] exitChoices = null, TooltipCallback[] exitCallbacks = null, 
+										TooltipCallback onCreation = null, TooltipCallback onDestruction = null) {
 
 			// Look for Tooltips component in the scene
 			Tooltips t = FindObjectOfType<Tooltips>();
@@ -56,12 +59,14 @@ namespace SaFrLib {
 			if (t.prefab == null) {
 				t.prefab = Resources.Load<GameObject>(defaultPrefabPath);
 			}
+			// Instantiate tooltip
 			GameObject newTooltip = Instantiate<GameObject>(t.prefab) as GameObject;
 			// Make sure new tooltip has Tooltip component
 			if (newTooltip.GetComponent<Tooltip>() == null) {
 				newTooltip.AddComponent<Tooltip>();
 			}
 			Tooltip tooltip = newTooltip.GetComponent<Tooltip>();
+			RectTransform tooltipRecTransform = newTooltip.GetComponent<RectTransform>();
 
 			// OnCreate callback
 			if (onCreation != null) {
@@ -75,12 +80,34 @@ namespace SaFrLib {
 			if (parent == null) {
 				Canvas c = FindObjectOfType<Canvas>();
 				if (c == null) {
+					// Create Canvas and EventSystem if none exists yet
 					c = new GameObject("Canvas (Created by Tooltips.cs)", typeof(Canvas), typeof(CanvasScaler), typeof(GraphicRaycaster)).GetComponent<Canvas>();
 					c.renderMode = RenderMode.ScreenSpaceOverlay;
 					GameObject g = new GameObject("Event System (Created by Tooltips.cs)", typeof(EventSystem), typeof(StandaloneInputModule));
 				}
 				parent = c.GetComponent<RectTransform>();
 			}
+			newTooltip.transform.SetParent(parent, false);
+
+			// Position new Tooltip
+			// First, account for default position
+			if (position == default(Vector3)) {
+				position.x = Screen.width / 2f;
+				position.y = Screen.height / 2f;
+			}
+			// If toFollow is specified, override default position
+			if (toFollow != null) {
+				position = Camera.main.WorldToScreenPoint(toFollow.position);
+				// Move by offset
+				if (offset == default(Vector3)) offset = Vector3.zero;
+				position += offset;
+				// Save to Tooltip
+				tooltip.toFollow = toFollow;
+				tooltip.toFollowOffset = offset;
+			}
+			tooltipRecTransform.position = position;
+
+			// Create exit buttons
 
 		}
 
