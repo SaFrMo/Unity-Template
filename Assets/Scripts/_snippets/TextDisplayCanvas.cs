cbb78eb3251a4e4da94fb0d96af4d9dcba7ee6a2
Sander <sandermoolin@gmail.com>
2016-09-19 14:56:22 -0400

    Revert "Revert "WIP on text display""
    This reverts commit 185924df3d522652b04dc4f28923da880e33a4e8.

---
+++ Assets/Scripts/_snippets/TextDisplayCanvas.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace SaFrLib {
+	public class TextDisplayCanvas : MonoBehaviour {
+
+		protected string[] content;
+
+		public virtual void SetContent(string[] newContent, bool displayImmediately = true) {
+			content = newContent;
+
+			if (displayImmediately) {
+				DisplayContent();
+			}
+		}
+
+		public virtual void DisplayContent() {
+
+		}
+	}
+}
