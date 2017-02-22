6408cd466d92f5d9fa1f352b3ad67a62c47d04a5
Sander <sandermoolin@gmail.com>
2017-02-22 09:41:38 -0500

    Added automatic movement in Standard2dMover (MoveTo, StopMovement)

--- Assets/Scripts/_snippets/Standard2dMover.cs
+++ Assets/Scripts/_snippets/Standard2dMover.cs
@@ -12,7 +12,8 @@ public class Standard2dMover : MonoBehaviour {
 	/// 
 	/// HOW:
 	/// 	Attach to a GameObject. That's it! The Standard2dMover either uses the Rigidbody2D/Rigidbody
-	/// 	you've attached to the GameObject, or it creates a Rigidbody2D to use on its own.
+	/// 	you've attached to the GameObject, or it creates a Rigidbody2D to use on its own. Remember to
+	/// 	turn gravity on or off as desired.
 	/// 
 
 	//
@@ -21,6 +22,11 @@ public class Standard2dMover : MonoBehaviour {
 	/// The movement speed of this GameObject.
 	/// </summary>
 	public float speed = 3f;
+
+	/// <summary>
+	/// Whether or not this Mover accepts user input (WASD).
+	/// </summary>
+	public bool inputEnabled = false;
 	/// <summary>
 	/// Whether this GameObject smooths out its movement using Input.GetAxis or not.
 	/// </summary>
@@ -33,6 +39,9 @@ public class Standard2dMover : MonoBehaviour {
 	protected Rigidbody rigid;
 	protected Rigidbody2D rigid2d;
 
+	protected Coroutine currentCoroutine;
+	public delegate void MovementCallback2d();
+
 	protected virtual void Start() {
 		// Check for existing Rigidbody2D or Rigidbody
 		rigid2d = GetComponent<Rigidbody2D>();
@@ -44,6 +53,10 @@ public class Standard2dMover : MonoBehaviour {
 		}
 	}
 
+	/// <summary>
+	/// Move the specified amount.
+	/// </summary>
+	/// <param name="movement">Movement.</param>
 	protected virtual void Move(Vector2 movement) {
 		if (rigid2d) {
 			rigid2d.MovePosition((Vector2)transform.position + movement);
@@ -53,15 +66,69 @@ public class Standard2dMover : MonoBehaviour {
 		}
 	}
 
-	public virtual void Update() {
-		// Capture smoothed/unsmoothed input
-		Vector2 input;
-		if (smoothMovement) {
-			input = new Vector2 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"));
-		} else {
-			input = new Vector2 (Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+	/// <summary>
+	/// Moves to a specified point, firing a callback on completion.
+	/// </summary>
+	/// <param name="position">Position.</param>
+	/// <param name="onComplete">On complete.</param>
+	/// <param name="interruptCurrentMovement">If set to <c>true</c> interrupt current movement.</param>
+	public virtual void MoveTo(Vector2 position, MovementCallback2d onComplete = null, float minDistance = 0.01f, bool interruptCurrentMovement = true) {
+		// Stop current movement if desired
+		if (interruptCurrentMovement && currentCoroutine != null) {
+			StopAutomaticMovement ();
 		}
 
-		Move(input * speed * Time.deltaTime);
+		currentCoroutine = StartCoroutine (MoveToCoroutine (position, onComplete, minDistance));
+	}
+
+	/// <summary>
+	/// Stops the automatic movement.
+	/// </summary>
+	public virtual void StopAutomaticMovement() {
+		if (currentCoroutine != null) {
+			StopCoroutine (currentCoroutine);
+			currentCoroutine = null;
+		}
+	}
+
+	/// <summary>
+	/// Movement coroutine. Approaches a specified point, then fires a callback on completion.
+	/// </summary>
+	/// <returns>The to coroutine.</returns>
+	/// <param name="target">Target.</param>
+	/// <param name="onComplete">On complete.</param>
+	/// <param name="minDistance">Minimum distance.</param>
+	protected virtual IEnumerator MoveToCoroutine(Vector2 target, MovementCallback2d onComplete, float minDistance){
+		while (!SaFrMo.CloseEnough2d ((Vector2)transform.position, target, minDistance)) {
+			Vector2 nextPoint = Vector2.MoveTowards ((Vector2)transform.position, target, speed * Time.deltaTime);
+			if (rigid2d != null) {
+				rigid2d.MovePosition (nextPoint);
+			} else {
+				rigid.MovePosition ((Vector3)nextPoint);
+			}
+			yield return new WaitForEndOfFrame ();
+		}
+
+		// Fire callback if specified
+		if (onComplete != null) {
+			onComplete.Invoke ();
+		}
+
+		// Clean out current coroutine reference
+		currentCoroutine = null;
+	}
+
+	public virtual void Update() {
+		if (inputEnabled) {
+			// Capture smoothed/unsmoothed input
+			Vector2 input;
+			if (smoothMovement) {
+				input = new Vector2 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"));
+			} else {
+				input = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+			}
+
+			Move (input * speed * Time.deltaTime);
+		}
 	}
 }
